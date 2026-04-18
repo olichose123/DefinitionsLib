7fee8053912fc341294ca5958931cbce0b5abfd8
Olivier Banville <olivier@banville.net>
2026-04-18 00:13:17 -0400

    replace manual resolver with automatic

--- DefinitionTypeResolver.cs
+++ DefinitionTypeResolver.cs
@@ -1,19 +1,14 @@
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
 
 namespace Definitions
 {
     /// <summary>
-    /// A Type Resolver for the Definition class. It allows for polymorphic serialization and deserialization of definitions, by using a type discriminator property called "$type". When a new definition type is encountered, it is added to the list of derived types for the Definition base type, allowing for correct serialization and deserialization of that type in the future. This allows for easy extension of the definition system with new types without needing to modify existing code.
+    /// A Type Resolver for the Definition class. It overrides the GetTypeInfo method to add polymorphism options for the Definition class, allowing for derived types of Definition to be deserialized based on a type discriminator property in the JSON. The derived types are added to the polymorphism options based on the types that are found in the executing assembly that are subclasses of Definition. This allows for easy deserialization of JSON into the correct derived type of Definition based on the type discriminator property in the JSON.
     /// </summary>
     public class DefinitionTypeResolver : DefaultJsonTypeInfoResolver
     {
-        static List<JsonDerivedType> DerivedTypes = new List<JsonDerivedType>();
-        public static void AddDerivedType(Type derivedType, string discriminator)
-        {
-            DerivedTypes.Add(new JsonDerivedType(derivedType, discriminator));
-        }
-
         public override JsonTypeInfo GetTypeInfo(Type type, JsonSerializerOptions options)
         {
             JsonTypeInfo jsonTypeInfo = base.GetTypeInfo(type, options);
@@ -27,9 +22,12 @@ namespace Definitions
                     IgnoreUnrecognizedTypeDiscriminators = false,
                     UnknownDerivedTypeHandling = System.Text.Json.Serialization.JsonUnknownDerivedTypeHandling.FailSerialization,
                 };
-                foreach (var derivedType in DerivedTypes)
+
+                var types = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsSubclassOf(typeof(Definition)));
+
+                foreach (var derivedType in types)
                 {
-                    jsonTypeInfo.PolymorphismOptions.DerivedTypes.Add(derivedType);
+                    jsonTypeInfo.PolymorphismOptions.DerivedTypes.Add(new JsonDerivedType(derivedType, derivedType.Name.ToLowerInvariant()));
                 }
             }
 
