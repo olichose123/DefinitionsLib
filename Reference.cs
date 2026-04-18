22aa9c704094f9eb6283fad8ce2d0886eb5a3405
Olivier Banville <olivier@banville.net>
2026-04-18 00:13:26 -0400

    document reference class

--- Reference.cs
+++ Reference.cs
@@ -6,6 +6,9 @@ namespace Definitions
     /// <typeparam name="T"></typeparam>
     public class Reference<T> where T : Definition
     {
+        /// <summary>
+        /// The name of the definition that this reference points to. Setting the name will invalidate the cached instance, causing it to be reloaded from the registered definitions the next time Get is called. If the name is null, Get will return null. This allows for references to definitions that may not exist at the time of reference creation, and for definitions to be loaded on demand.
+        /// </summary>
         public string? Name
         {
             get
@@ -32,9 +35,13 @@ namespace Definitions
             instance = definition;
         }
 
+        /// <summary>
+        /// Gets the definition instance that this reference points to. If the instance has already been loaded and the name has not changed, it returns the cached instance. If the name is null, it returns null. Otherwise, it loads the definition from the registered definitions using the name and caches it before returning it. This allows for lazy loading of definitions and for references to be created before the definitions they point to are registered.
+        /// </summary>
+        /// <returns></returns>
         public T? Get()
         {
-            if (instance != null)
+            if (instance != null && instance.Name == name)
             {
                 return instance;
             }
